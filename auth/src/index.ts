73c7878fce83d2aa8975bcdbc4128cc1d9e9fb74
Justin Chan <jchan922@gmail.com>
2020-11-01 12:59:01 -0800

    add remote build on google and jwt for sign up

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express = require('express');
+import mongoose = require('mongoose');
 import 'express-async-errors';
 import { json } from 'body-parser';
+import cookieSession = require("cookie-session");
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/sign-in';
@@ -10,7 +12,12 @@ import { errorHandler } from './middleware/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true  
+}));
 
 app.use(currentUserRouter);
 app.use(signInRouter);
@@ -23,6 +30,23 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!');
-});
+const start = async () => {
+    if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
+
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.log(err);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!');
+    });
+};
+
+start();
--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,8 +1,11 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -24,10 +27,30 @@ router.post('/api/users/signup', [
         
         const { email, password } = req.body;
 
-        console.log('Creating a user');
-        throw new DatabaseConnectionError();
+        const existingUser = await User.findOne({ email });
 
-        res.send({});
+        if (existingUser) {
+            throw new BadRequestError('Email in use');
+        }
+
+        const user = User.build({ email, password });
+        await user.save();
+
+        // generate jwt
+        const userJwt = jwt.sign(
+            {
+                id: user.id,
+                email: user.email
+            },
+            process.env.JWT_KEY!
+        );
+
+        // store on session object
+        req.session = {
+            jwt: userJwt
+        };
+
+        res.status(201).send(user);
     }
 );
 
