420598c3ab3dd06a88240d0937308c9a49b60344
Justin Chan <jchan922@gmail.com>
2020-11-07 19:18:22 -0800

    add global getInitialProps and signout to client

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,10 +3,35 @@
  * https://github.com/vercel/next.js/blob/master/errors/css-global.md
  */
 
+import buildClient from '../api/build-client';
+import Header from '../components/header';
+
 import 'bootstrap/dist/css/bootstrap.css';
 
-const _app = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
-};
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+    return (
+        <div>
+            <Header currentUser={currentUser} />
+            <Component {...pageProps} />
+        </div>
+    );
+}; 
 
-export default _app;
+// Next.js method for SSR rending
+AppComponent.getInitialProps = async appContext => {
+    const { Component, ctx } = appContext;
+    const client = buildClient(ctx)
+    const { data } = await client.get('/api/users/currentuser');
+    
+    let pageProps = {};
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return {
+        pageProps,
+        ...data
+    };
+ };
+
+export default AppComponent;
