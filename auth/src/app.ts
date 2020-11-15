a0025a46ff2b8a23d5c855cef9b9019a1b7ad6f6
Justin Chan <jchan922@gmail.com>
2020-11-15 13:18:46 -0800

    removed shared code and referrenced shared npm @jmcticketing/common module now.

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -7,8 +7,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/sign-in';
 import { signOutRouter } from './routes/sign-out';
 import { signUpRouter } from './routes/sign-up';
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { errorHandler, NotFoundError } from '@jmctickets/common';
 
 const app = express();
 app.set('trust proxy', true);
--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -2,10 +2,9 @@ import * as express from 'express';
 import { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+import { validateRequest, BadRequestError } from '@jmctickets/common';;
 
-import { validateRequest } from '../middleware/validate-request';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
